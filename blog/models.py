944e2107143ca706bbd39de32e2dfd518bf5e936
Nowee <nowee018@naver.com>
2022-10-16 12:02:28 +0900

    포스트 목록 / 상세 페이지에 첨부파일, 미리보기, 필터 적용하기

--- blog/models.py
+++ blog/models.py
@@ -12,14 +12,8 @@ class Post(models.Model):
 
     head_image = models.ImageField(
         upload_to='blog/images/%Y/%m/%d/', blank=True)
-<<<<<<< HEAD
-    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
-=======
-    # %Y 2022, %y 22
-    file_upload = models.FileField(
-        upload_to='blog/files/%Y/%m/%d/', blank=True)
 
->>>>>>> e29d0cd0b06e226a0839689b8731669d43210c43
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
