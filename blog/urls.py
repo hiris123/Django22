23003952c2d14fcc91c4ce79e1e9d27d906e3381
nowee018 <nowee018@naver.com>
2022-11-10 12:16:21 +0900

    포스트 수정페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [  # IP주소/blog/
     path('', views.PostList.as_view()),
     path('create_post/',views.PostCreate.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()), # IP주소/blog/category/slug/
+    path('update_post/<int:pk>/',views.PostUpdate.as_view()), #IP주소/blog/update_post/slug/
     path('tag/<str:slug>/',views.tag_page),# IP주소/blog/tag/slug
 
 
