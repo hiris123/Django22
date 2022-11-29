055eaecb98c22bb78026e920668062ebf24856fd
nowee018 <nowee018@naver.com>
2022-11-29 15:55:14 +0900

    대문자 페이지 완성하기

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from blog.models import Post
+
+
 
 # Create your views here.
-
-
 def landing(request):
-    return render(request, 'single_pages/landing.html')
+    recent_post = Post.objects.order_by('-pk')[:3] # 0,1,2 까지만 가져오겠다.
+    return render(request, 'single_pages/landing.html', {
+        'recent_posts' : recent_post,
+    })
 
 
 def about_me(request):
